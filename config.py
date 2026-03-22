f15230de251ceaaea29e353da9f5f165738a5658
DEVILJO2018 <deviljo2018@gmail.com>
2026-03-23 00:53:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -68,7 +68,10 @@ BOT_STATS_TEXT = "<b>BOT UPTIME :</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Sry You can't Able to Message me !\n\n» My Owner 👉 "
 
 START_MSG = """
-Hi {first} Friend! I am an Advance File Store bot 😈
+Hi {first} Friend! I am an Advance File Store bot.
+
+I was created by 👉 <a href='https://t.me/KejoRealm'>@KejoRealm</a>
+"""
 
 I was created by 👉 <a href='https://t.me/KejoRealm'>@KejoRealm</a>
 """
